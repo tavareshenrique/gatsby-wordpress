c81f74b406d36a6473d91834585303191f22f3e2
Henrique Tavares <ihenrits@gmail.com>
2019-08-05 20:54:18 -0300

    siteMetadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,9 +4,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Henrique Tavares`,
+    description: `this is my code life`,
+    author: `@ihenrits`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
