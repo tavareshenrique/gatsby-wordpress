63a9ca4807e03cc1ca0c691c303bba5f2bc2d349
Henrique Tavares <ihenrits@gmail.com>
2019-08-05 20:52:45 -0300

    configuration gatsby-source-wordpress

--- gatsby-config.js
+++ gatsby-config.js
@@ -31,6 +31,26 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: 'gatsby-source-wordpress',
+      options: {
+        baseUrl: 'henriquetavares.home.blog',
+        protocol: 'https',
+        hostingWPCOM: true,
+        useACF: false,
+        auth: {
+          wpcom_app_clientSecret: process.env.WORDPRESS_SECRET,
+          wpcom_app_clientId: process.env.WORDPRESS_CLIENTID,
+          wpcom_user: process.env.WORDPRESS_USERNAME,
+          wpcom_pass: process.env.WORDPRESS_PASSWORD,
+        },
+        verboseOutput: false,
+        searchAndReplaceContentUrls: {
+          sourceUrl: 'https://henriquetavares.home.blog',
+          replacementUrl: 'https://henriquetavares.home.blog',
+        },
+      },
+},
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
